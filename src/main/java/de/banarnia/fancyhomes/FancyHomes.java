2a462cc1bfc96caabfe18493c86cae2f8d9a28c4
Mike Nipkow <nipkow.mike@outlook.com>
2023-09-19 14:40:07 +0200

    Added general spigot configuration.

---
+++ src/main/java/de/banarnia/fancyhomes/FancyHomes.java
@@ -0,0 +1,11 @@
+package de.banarnia.fancyhomes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FancyHomes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
