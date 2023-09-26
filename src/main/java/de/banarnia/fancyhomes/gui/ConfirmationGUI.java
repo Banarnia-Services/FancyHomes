4aacfc0a7eae6e773e851bb146502fa86d29fa2f
Mike Nipkow <nipkow.mike@outlook.com>
2023-09-26 08:50:38 +0200

    Port ConfirmationGUI to new GUI api

--- src/main/java/de/banarnia/fancyhomes/gui/ConfirmationGUI.java
+++ src/main/java/de/banarnia/fancyhomes/gui/ConfirmationGUI.java
@@ -1,66 +1,95 @@
 package de.banarnia.fancyhomes.gui;
 
-import com.github.stefvanschie.inventoryframework.gui.GuiItem;
-import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
-import com.github.stefvanschie.inventoryframework.pane.OutlinePane;
-import com.github.stefvanschie.inventoryframework.pane.Pane;
-import de.banarnia.fancyhomes.api.ItemBuilder;
+import dev.triumphteam.gui.builder.item.ItemBuilder;
+import dev.triumphteam.gui.components.GuiType;
+import dev.triumphteam.gui.components.util.Legacy;
+import dev.triumphteam.gui.guis.Gui;
+import dev.triumphteam.gui.guis.GuiItem;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
+import java.util.List;
 import java.util.function.Consumer;
 
-public class ConfirmationGUI extends ChestGui {
+public class ConfirmationGUI {
 
-    public static void open(Player player, String title, Consumer<Boolean> acceptOrDeny) {
-        new ConfirmationGUI(title, player, acceptOrDeny).show(player);
+    private String title;
+    private String acceptItemName;
+    private List<String> acceptItemLore;
+    private String denyItemName;
+    private List<String> denyItemLore;
+    private Sound acceptSound;
+    private Sound denySound;
+    private Consumer<Boolean> consumer;
+
+    private Gui gui;
+
+    public ConfirmationGUI(String title, String acceptItemName, List<String> acceptItemLore,
+                           String denyItemName, List<String> denyItemLore,
+                           Sound acceptSound, Sound denySound, Consumer<Boolean> consumer) {
+        this.title = title;
+        this.acceptItemName = acceptItemName != null ? acceptItemName : "§2✔";
+        this.acceptItemLore = acceptItemLore;
+        this.denyItemName = denyItemName != null ? denyItemName : "§4✖";
+        this.denyItemLore = denyItemLore;
+        this.acceptSound = acceptSound != null ? acceptSound : Sound.ENTITY_EXPERIENCE_ORB_PICKUP;
+        this.denySound = denySound != null ? denySound : Sound.BLOCK_NOTE_BLOCK_BANJO;
+        this.consumer = consumer;
+
+        this.gui = Gui.gui(GuiType.HOPPER).title(Legacy.SERIALIZER.deserialize(title)).create();
+        this.gui.setDefaultClickAction(event -> event.setCancelled(true));
+        this.gui.setCloseGuiAction(event -> deny((Player) event.getPlayer()));
+        init();
     }
 
-    private Consumer<Boolean> acceptOrDeny;
+    public ConfirmationGUI(String title, Consumer<Boolean> consumer) {
+        this(title, null, null, null, null, null, null, consumer);
+    }
 
-    public ConfirmationGUI(String title, Player player, Consumer<Boolean> acceptOrDeny) {
-        super(3, title);
-        this.acceptOrDeny = acceptOrDeny;
+    public ConfirmationGUI(String title, String acceptItemName, String denyItemName, Consumer<Boolean> consumer) {
+        this(title, acceptItemName, null, denyItemName, null, null, null, consumer);
+    }
 
-        setOnGlobalClick(event -> event.setCancelled(true));
-        setOnClose(event -> deny(player));
+    public void init() {
+        gui.getFiller().fill(ItemBuilder.from(Material.GRAY_STAINED_GLASS_PANE).setName(" ").asGuiItem());
 
-        OutlinePane background = new OutlinePane(0, 0, 9, 3, Pane.Priority.LOWEST);
-        background.addItem(new GuiItem(ItemBuilder.of(Material.GRAY_STAINED_GLASS_PANE).name(" ").build()));
-        background.setRepeat(true);
-        addPane(background);
+        GuiItem acceptItem = ItemBuilder.from(Material.EMERALD_BLOCK)
+                .setName(acceptItemName)
+                .setLore(acceptItemLore)
+                .asGuiItem();
+        acceptItem.setAction(click -> accept((Player) click.getWhoClicked()));
+        gui.setItem(2, acceptItem);
 
-        OutlinePane accept = new OutlinePane(2, 1, 1, 1);
-        ItemBuilder builder = ItemBuilder.of(Material.EMERALD_BLOCK).name("§2✔");
-        accept.addItem(new GuiItem(builder.build(), click -> accept(player)));
-        addPane(accept);
+        GuiItem denyItem = ItemBuilder.from(Material.REDSTONE_BLOCK)
+                .setName(denyItemName)
+                .setLore(denyItemLore)
+                .asGuiItem();
+        denyItem.setAction(click -> deny((Player) click.getWhoClicked()));
+        gui.setItem(6, denyItem);
+    }
 
-        OutlinePane deny = new OutlinePane(6, 1, 1, 1);
-        builder = ItemBuilder.of(Material.REDSTONE_BLOCK).name("§4✖");
-        deny.addItem(new GuiItem(builder.build(), click -> {
-            deny(player);
-        }));
-        addPane(deny);
+    public void open(Player player) {
+        this.gui.open(player);
     }
 
     private void accept(Player player) {
-        if (this.acceptOrDeny == null)
+        if (this.consumer == null)
             return;
 
-        Consumer<Boolean> consumer = this.acceptOrDeny;
-        this.acceptOrDeny = null;
-        player.playSound(player, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+        Consumer<Boolean> consumer = this.consumer;
+        this.consumer = null;
+        player.playSound(player, acceptSound, 1, 1);
         consumer.accept(true);
     }
 
     private void deny(Player player) {
-        if (this.acceptOrDeny == null)
+        if (this.consumer == null)
             return;
 
-        player.playSound(player, Sound.BLOCK_NOTE_BLOCK_BANJO, 1, 1);
-        this.acceptOrDeny.accept(false);
-        this.acceptOrDeny = null;
+        player.playSound(player, denySound, 1, 1);
+        this.consumer.accept(false);
+        this.consumer = null;
     }
 
 }
